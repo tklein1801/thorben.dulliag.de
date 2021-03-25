8645e94647823cdc5f8c119f225a3ba905205898
Thorben <38816229+tklein1801@users.noreply.github.com>
2021-03-26 00:39:01 +0100

    Add qoute

--- src/App.js
+++ src/App.js
@@ -1,51 +1,17 @@
 import { Component } from "react";
 import Link from "./components/Link";
-import { links } from "./data/links.json";
+import { name, quote, links } from "./data/content.json";
 import Me from "./images/me.jpg";
 import "./style/_master.scss";
 
 export default class App extends Component {
-  constructor() {
-    super();
-    this.state = { username: "thorben.klein", followers: 0, follows: 0 };
-  }
-
-  async getInstagramProfile() {
-    const { username } = this.state;
-    var response = await fetch(`https://www.instagram.com/${username}/?__a=1`);
-    var data = await response.json();
-    return data;
-  }
-
-  async componentDidMount() {
-    // var instagramResponse = await this.getInstagramProfile();
-    // followers => edge_followed_by.count
-    // follows => edge_follow.count
-    // this.setState({
-    //   followers: instagramResponse.edge_followed_by.count,
-    //   follows: instagramResponse.edge_follow.count,
-    // });
-  }
-
   render() {
-    const { followers, follows } = this.state;
     return (
       <div className="wrapper">
         <div className="container">
-          <img className="me" src={Me} alt="Image of mine" />
-          <h2 className="username">Thorben Klein</h2>
-
-          {/* <div className="follower-container">
-            <div className="follower">
-              <p className="label">Followers</p>
-              <p className="count">{followers}</p>
-            </div>
-            <div className="follows">
-              <p className="label">Follows</p>
-              <p className="count">{follows}</p>
-            </div>
-          </div> */}
-
+          <img className="me" src={Me} alt="Picture of mine" />
+          <h2 className="username">{name}</h2>
+          <blockquote>{quote}</blockquote>
           <div className="button-container">
             {links.map((link, index) => {
               return <Link key={index} text={link.name} target={link.target} />;
